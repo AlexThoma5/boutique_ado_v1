ec0ce483331a35a964523f4d9a74b90025d13b1c
Alex <alexthomas1912@gmail.com>
2026-01-04 02:36:05 +0000

    Add Stripe webhook secret key variable

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -29,7 +29,10 @@ SECRET_KEY = 'django-insecure-k&o9$wnul6=_1qpmcu$&s_6%@kz7m5@m+@=v7@av+c7vj21u_p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition
@@ -182,3 +185,4 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get("STRIPE_PUBLIC_KEY", '')
 STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY", '')
+STRIPE_WH_SECRET = os.environ.get("STRIPE_WH_SECRET", '')
