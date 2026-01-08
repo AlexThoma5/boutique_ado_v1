286e590bd509a7da5c7d5a74b7a532fb46fbedd7
Alex <alexthomas1912@gmail.com>
2026-01-08 02:19:10 +0000

    Comments on product form for clarity

--- products/forms.py
+++ products/forms.py
@@ -3,16 +3,35 @@ from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
+    """
+    A form that automatically creates fields
+    based on the Product model
+    """
 
     class Meta:
-        model = Product
-        fields = '__all__'
+        model = Product              # Use the Product model
+        fields = '__all__'            # Include all model fields
 
     def __init__(self, *args, **kwargs):
+        # First, let Django build the default form
         super().__init__(*args, **kwargs)
-        categories = Category.objects.all()
-        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
+        # Get all categories from the database
+        categories = Category.objects.all()
+
+        # Create a list of (id, friendly_name) tuples
+        # This is the format Django expects for dropdown choices
+        # c = individual category
+        friendly_names = [
+            (c.id, c.get_friendly_name())
+            for c in categories
+        ]
+
+        # Replace the default category dropdown choices
+        # with our friendly names
         self.fields['category'].choices = friendly_names
+
+        # Add the same CSS classes to every form field
+        # so styling is consistent
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'border-black rounded-0'
