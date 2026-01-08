62744129beed89be4707890a6354749367f33e70
Alex <alexthomas1912@gmail.com>
2026-01-08 02:37:43 +0000

    Product admin - Add product view, url and template

--- products/models.py
+++ products/models.py
@@ -12,7 +12,7 @@ class Category(models.Model):
         return self.name
 
     def get_friendly_name(self):
-        return self.name
+        return self.friendly_name
 
 
 class Product(models.Model):
